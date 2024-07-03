9985c34a5f4e4bf6099ead75212112ca504aad87
Ushaxoxo <nandhiniusha2003@gmail.com>
2024-07-04 01:24:13 +0530

    sign up integration and authentication

--- src/main/java/com/example/StockUp/service/FirebaseInitialization.java
+++ src/main/java/com/example/StockUp/service/FirebaseInitialization.java
@@ -14,7 +14,7 @@ public class FirebaseInitialization {
     public void initialize(){
        try {
            FileInputStream serviceAccount =
-                   new FileInputStream("src/blank.json");
+                   new FileInputStream("src/serviceAccountKey.json");
 
            FirebaseOptions options = new FirebaseOptions.Builder()
                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
